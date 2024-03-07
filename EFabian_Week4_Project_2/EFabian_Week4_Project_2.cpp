ca7e73c8214153adb9137fe21e7eaa3644eafd48
efabian6 <efabian6@WRL-00L123-L018>
2024-03-07 15:55:53 -0600

    Add project files.

---
+++ EFabian_Week4_Project_2/EFabian_Week4_Project_2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Table of 5\n\n";
+	int x = 1;
+	while (x<=12)
+	{
+		cout << "5 x " << x << " = " << (5 * x) << endl;
+		x++;
+	}
+}
